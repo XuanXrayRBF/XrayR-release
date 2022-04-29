01decb14ad51f0879821049b5b68744d042e524b
轩宝 <77476279+XuanXrayRBF@users.noreply.github.com>
2022-04-29 13:18:20 +0800

    Add files via upload

--- XrayR.sh
+++ XrayR.sh
@@ -84,7 +84,7 @@ before_show_menu() {
 }
 
 install() {
-    bash <(curl -Ls https://raw.githubusercontent.com/XuanXrayRBF/XrayR-release/master/install.sh) $version
+    bash <(curl -Ls https://raw.githubusercontent.com/XuanXrayRBF/XrayR-release/master/install.sh)
     if [[ $? == 0 ]]; then
         if [[ $# == 0 ]]; then
             start
