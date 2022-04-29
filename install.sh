f2ed7d71f0ff788994017b3534fb363b19f5cbdb
轩宝 <77476279+XuanXrayRBF@users.noreply.github.com>
2022-04-29 13:34:48 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -136,7 +136,7 @@ install_XrayR() {
     rm /etc/systemd/system/XrayR.service -f
     file="https://github.com/XuanXrayRBF/XrayR-release/raw/master/XrayR.service"
     wget -q -N --no-check-certificate -O /etc/systemd/system/XrayR.service ${file}
-    #cp -f XrayR.service /etc/systemd/system/
+    cp -f XrayR.service /etc/systemd/system/
     systemctl daemon-reload
     systemctl stop XrayR
     systemctl enable XrayR
